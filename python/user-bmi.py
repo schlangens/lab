53e8181413f48741e8507fbde82475cdb0aa0aa7
Scott Schlangen <sschlangen@gmail.com>
2025-01-22 20:28:54 -0500

    bmi-user-bmi

---
+++ python/user-bmi.py
@@ -0,0 +1,8 @@
+height = float(input("Enter your height in inches: "))
+weight = float(input("Enter your weight in pounds: "))
+
+
+bmi = (weight / (height ** 2)) * 703
+
+print("Your BMI is:", bmi)
+
